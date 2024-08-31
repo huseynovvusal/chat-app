db8cfb91d030872cff5ceb3f047d4d4c8df527ec
huseynovvusal <vslh333222111@gmail.com>
2024-08-31 17:17:14 +0400

    Users

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,13 +1,49 @@
 import React from "react"
 import SearchBar from "./SearchBar"
-import Profile from "./Profile"
+import User from "./User"
+
+const users = [
+  {
+    firstName: "Mirze",
+    lastName: "Guleliyev",
+    username: "mgleliyevv",
+    profilePicture:
+      "https://avatar.iran.liara.run/username?username=Mirze+Guleliyev",
+  },
+  {
+    firstName: "Hidayat",
+    lastName: "Hasanzade",
+    username: "hidayet_atu",
+    profilePicture:
+      "https://avatar.iran.liara.run/username?username=Hidayet+Hasanzade",
+  },
+  {
+    firstName: "Orxan",
+    lastName: "Memisov",
+    username: "orxan_muellim",
+    profilePicture:
+      "https://avatar.iran.liara.run/username?username=Orxan+Memisov",
+  },
+]
 
 export default function SideBar() {
   return (
-    <div className="w-full h-full py-6 px-4 flex flex-col gap-6">
-      <Profile />
+    <div className="w-full h-full py-6 px-4 flex flex-col gap-6 overflow-y-scroll">
+      <User
+        firstName="Vusal"
+        lastName="Huseynov"
+        username="huseynovvusal"
+        profilePicture="https://avatar.iran.liara.run/username?username=Vusal+Huseynov"
+        disabled
+      />
 
       <SearchBar />
+
+      <div className="flex flex-col gap-4">
+        {users.map((user) => (
+          <User key={user.username} {...user} />
+        ))}
+      </div>
     </div>
   )
 }
---
+++ client/src/components/User.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+export interface UserProps {
+  firstName: string
+  lastName: string
+  username: string
+  profilePicture: string
+  disabled?: boolean
+}
+
+export default function User({
+  firstName,
+  lastName,
+  username,
+  profilePicture,
+  disabled = false,
+}: UserProps) {
+  return (
+    <div
+      className={`flex items-center gap-3 select-none border-[1px] border-transparent rounded-xl ${
+        !disabled && "hover:border-slate-200 px-2 py-2 cursor-pointer"
+      }`}
+    >
+      <img
+        className="w-12 h-12 rounded-full pointer-events-none border-2 border-transparent outline-dashed outline-2 outline-slate-400"
+        src={profilePicture}
+      />
+      <div className="flex flex-col">
+        <span className="font-medium text-base text-slate-800">
+          {firstName} {lastName}
+        </span>
+        <span className="font-regular text-xs text-slate-600">@{username}</span>
+      </div>
+    </div>
+  )
+}
