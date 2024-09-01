81ce76a3855e40ce082dc17626d856d1adaaadbe
huseynovvusal <vslh333222111@gmail.com>
2024-09-01 12:16:22 +0400

    Some Fixes

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 import { SendIcon } from "lucide-react"
-import React, { useEffect, useRef } from "react"
+import { useRef } from "react"
 import { useParams } from "react-router-dom"
 
 export default function Chat() {
@@ -29,20 +29,25 @@ export default function Chat() {
   }
 
   return (
-    <div className="w-full h-full flex flex-col justify-between px-6">
-      <div className="w-full h-full bg-red-300"></div>
+    <div className="w-full h-full flex flex-col justify-between">
+      {/* <div className="w-full h-full bg-red-300"></div> */}
 
-      <div className="flex rounded-[20px] gap-4 mb-4 bg-slate-200 items-center">
-        <Textarea
-          ref={textAreaRef}
-          placeholder="Hello! How are you?"
-          rows={1}
-          className="resize-none border-none overflow-y-auto py-4 focus-visible:ring-0 focus-visible:ring-offset-0  bg-transparent"
-          onInput={adjustHeight}
-        />
-        <Button variant="link">
-          <SendIcon size={24} />
-        </Button>
+      <div className="px-6">
+        <div className="flex rounded-[20px] px-2 gap-4 mb-4 bg-slate-200 items-center">
+          <Textarea
+            ref={textAreaRef}
+            placeholder="Hello! How are you?"
+            rows={1}
+            className="resize-none border-none overflow-y-auto py-4 focus-visible:ring-0 focus-visible:ring-offset-0  bg-transparent"
+            onInput={adjustHeight}
+          />
+          <Button
+            variant="default"
+            className="rounded-full min-w-10 min-h-10 flex items-center justify-center bg-slate-800 p-0"
+          >
+            <SendIcon size={16} />
+          </Button>
+        </div>
       </div>
     </div>
   )
